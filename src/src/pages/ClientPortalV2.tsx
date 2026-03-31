63c2dd14cb941db09806944d50845196b2e8860e
Kit <kit@axiomstreamgroup.com>
2026-03-31 18:41:28 +0000

    fix: rebuild dist with ryanh+markb PIN registry entries

---
+++ src/src/pages/ClientPortalV2.tsx
@@ -0,0 +1,2 @@
+import React from 'react'
+export default function ClientPortalV2() { return <div>Portal</div> }
---
+++ src/src/pages/RAGDemo.tsx
@@ -0,0 +1,2 @@
+import React from 'react'
+export default function RAGDemo() { return <div>Portal</div> }
